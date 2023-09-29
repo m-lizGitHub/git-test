baf430bc762b4695c60fe24afc8d70b89a92662b
lcz <lcz@mytest.cn>
2023-09-29 21:26:25 +0800

    first commit

---
+++ src/main/java/org/lcz/git/GitTest.java
@@ -0,0 +1,11 @@
+package org.lcz.git;
+
+/**
+ * @author lcz06
+ * @date 2023/9/29 - 21:19
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,Git!");
+    }
+}
